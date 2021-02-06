0cfaad7f5873bc41726f01fceda9bb578e3c7c56
sernguang <sernguang@gmail.com>
2021-02-06 19:16:18 +0800

    mydjangoreact

--- mydjangoreact/frontend/src/components/App.js
+++ mydjangoreact/frontend/src/components/App.js
@@ -1,17 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
-import HomePage from "./HomePage";
+
+// import { Provider as AlertProvider } from 'react-alert';
+// import AlertTemplate from 'react-alert-template-basic';
+
+import Header from "./layout/Header";
+import Dashboard from './leads/Dashboard';
+// import Alerts from './layout/Alerts';
+
+import { Provider } from 'react-redux';
+import store from '../store';
+
+// // Alert Options
+// const alertOptions = {
+//     timeout: 3000,
+//     position: 'top center'
+// }
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
-      <div>
-        <HomePage />
-      </div>
+        <Provider store={store}>
+            {/* <AlertProvider template={AlertTemplate}
+            {...alertOptions}> */}
+                <Fragment>
+                    <Header />
+                    {/* <Alerts /> */}
+                    <div className="container">
+                        <Dashboard />
+                    </div>
+                </Fragment>
+            {/* </AlertProvider> */}
+        </Provider>
     );
   }
 }
--- mydjangoreact/frontend/urls.py
+++ mydjangoreact/frontend/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
-from .views import index
+# from django.urls import path
+# from .views import index
 
+# urlpatterns = [
+#     path('', index),
+#     path('join', index),
+#     path('create', index),
+#     path('join/1', index)
+# ]
+
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('', index),
-    path('join', index),
-    path('create', index),
-    path('join/1', index)
+  path('', views.index)
 ]
